30213f7d9f04d1b0f75b9495ffe957c56175bd46
emmihr <46039037+emmihr@users.noreply.github.com>
2019-11-08 00:26:37 -0600

    Add files via upload

---
+++ pyralite-0.1.js
@@ -0,0 +1,84 @@
+window.elemByID = function elembyid(idinput) {
+  if (document.getElementById(idinput)) {
+    return document.getElementById(idinput);
+  } else {
+    return;
+  }
+};
+
+Array.prototype.first = function first() {
+  return this[0];
+};
+
+Array.prototype.last = function last() {
+  return this[this.length - 1];
+};
+
+Number.prototype.round = function(places) {
+  return +(Math.round(this + "e+" + places)  + "e-" + places);
+};
+
+window.elemsByTag = function elemsbytag(sel) {
+  if (document.querySelectorAll(sel)) {
+    return [].slice.call(document.querySelectorAll(sel));
+  } else {
+    return;
+  }
+};
+
+window.elemsByClass = function elemsbyclass(sel) {
+  if (document.querySelectorAll('.'+sel)) {
+    return [].slice.call(document.querySelectorAll('.'+sel));
+  } else {
+    return;
+  }
+};
+
+HTMLElement.prototype.innerhtml = function innerHTML(text) {
+ if (text) {
+   this.innerHTML = text.toString();
+   return this.innerHTML;
+ } else {
+   return this.innerHTML;
+ }
+};
+
+HTMLElement.prototype.outerhtml = function outerHTML(text) {
+ if (text) {
+   this.outerHTML = text.toString();
+   return this.outerHTML;
+ } else {
+   return this.outerHTML;
+ }
+};
+
+HTMLElement.prototype.text = function text(text) {
+ if (text) {
+   this.innerText = text.toString();
+   return this.innerText;
+ } else {
+   return this.innerText;
+ }
+};
+
+window.newelement = function newelement(tag,content) {
+  if (tag) {
+    let internalElement = document.createElement(tag);
+    if (content) {
+      internalElement.innerHTML = content;
+    }
+  }
+  return internalElement;
+};
+
+HTMLElement.prototype.defocus = function defocus() {
+  return this.blur();
+};
+
+HTMLElement.prototype.setfocus = function setfocus(boolean) {
+  if (boolean) {
+    this.focus();
+  } else if (!boolean) {
+    this.blur();
+  }
+};
