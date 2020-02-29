d904d2ad020aeaae19978539cd13aadb9cf6da27
Glitch (kel5isgod-pyralite) <none>
2020-02-29 21:46:48 +0000

    Fix JSDoc for setStyle

---
+++ pyralite-1.0.3.js
@@ -0,0 +1,162 @@
+/**
+* Alternative to document.getElementById.
+*
+* @param {String} id - ID to get element from.
+* @returns {HTMLElement}
+*/
+window.elemById = function elembyid(id) {
+    return document.getElementById(id) || null;
+};
+/**
+* Check if a number is even.
+* @augments Number
+* @returns {Boolean}
+*/
+Number.prototype.isEven = function() {
+    return !(this % 2); 
+};
+/**
+* Get the GCD (Greatest Common Denominator) of 2 numbers.
+* @augments Math
+* @param {Number} a
+* @param {Number} b
+* @returns {(Number|NaN)}
+*/
+Math.gcd = function gcd(a,b){
+  return b ? gcd(b, a%b) : a;
+};
+/**
+* Reduce a fraction (written as a/b). Makes use of {@link Math#gcd}.
+* @augments Math
+* @param {Number} numerator
+* @param {Number} denominator
+* @returns {Number[]}
+*/
+Math.reduce = function reduce(numerator,denominator){
+  if (isNaN(numerator) || isNaN(denominator)) return NaN;
+  let gcd = Math.gcd(numerator,denominator);
+  return [numerator/gcd, denominator/gcd];
+};
+/**
+* Shuffle an array.
+* @augments Array
+* @returns {Array}
+*/
+Array.prototype.shuffle = function shuffle() {
+  var currentIndex = this.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = this[currentIndex];
+    this[currentIndex] = this[randomIndex];
+    this[randomIndex] = temporaryValue;
+  }
+  return this;
+};
+/**
+* Return the first element of an array.
+* @augments Array
+* @returns {*}
+*/
+Array.prototype.first = function first() {
+  return this[0];
+};
+/**
+* Return the last element of an array.
+* @augments Array
+* @returns {*}
+*/
+Array.prototype.last = function last() {
+  return this[this.length - 1];
+};
+/**
+* Round a number to some places. Not to be confused with Math.round.
+* @augments Number
+* @param {Number} [places=2] - How many decimal places to round the number to. Default 2.
+* @returns {Number}
+*/
+Number.prototype.round = function(places=2) {
+  return +(Math.round(this + "e+" + places)  + "e-" + places);
+};
+/**
+* Gets elements by the provided tag (e.g. "h1", "em")
+* @param {String} tag - The tag to use.
+* @returns {HTMLElement[]}
+*/
+window.elemsByTag = function elemsbytag(tag) {
+    return [].slice.call(document.querySelectorAll(tag)) || null;
+};
+/**
+* Gets elements by the provided class (e.g. "big", "testclasss")
+* @param {String} class - The class to use.
+* @returns {HTMLElement[]}
+*/
+window.elemsByClass = function elemsbyclass(cls) {
+    return [].slice.call(document.querySelectorAll('.'+cls)) || null;
+};
+/**
+* Set the inner HTML of an element.
+* @augments HTMLElement
+* @param {String} text - The string to set the inner HTML to.
+* @returns {String}
+*/
+HTMLElement.prototype.innerHtml = function innerHTML(text) {
+ if (text) {
+   this.innerHTML = text.toString() || this.innerHTML;
+   return this.innerHTML;
+}};
+/**
+* Set the outer HTML of an element.
+* @augments HTMLElement
+* @param {String} text - The string to set the outer HTML to.
+* @returns {String}
+*/
+HTMLElement.prototype.outerHtml = function outerHTML(text) {
+ if (text) {
+   this.outerHTML = text.toString() || this.outerHTML;
+   return this.outerHTML;
+}};
+/**
+* Set the inner text of an HTML element.
+* @augments HTMLElement
+* @param {String} text - The string to set the inner text to.
+* @returns {String}
+*/
+HTMLElement.prototype.text = function text(text) {
+ if (text) {
+   this.innerText = text.toString() || this.innerText;
+   return this.innerText;
+}};
+
+/**
+* Defocus an HTML element. Alias for blur().
+* @augments HTMLElement
+* @returns {undefined}
+*/
+HTMLElement.prototype.defocus = function defocus() {
+  return this.blur();
+};
+/**
+* Set the focus of an HTML element on or off. Good for toggling.
+* @augments HTMLElement
+* @param {Boolean} boolean - Whether to focus or defocus the element.
+* @returns {undefined}
+*/
+HTMLElement.prototype.setFocus = function setfocus(boolean) {
+  if (boolean) {
+    this.focus();
+  }
+  if (!boolean) {
+    this.blur();
+  }
+};
+/**
+* Sets a style rule of an element.
+* @augments HTMLElement
+* @param {String} sty - The style to set.
+* @param {(String|Number)} - The value to set the style to.
+* @returns {undefined}
+*/
+HTMLElement.prototype.setStyle = function setstyle(sty, value) {
+  this.style[sty] = value;
+};
